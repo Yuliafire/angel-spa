cbcfefc209009614d68fad43ed4d66ee6f3746e4
Yulia Podgurskaia <linguavoice1@yahoo.com>
2025-09-03 17:36:39 +0300

    style: apply styles to the burger nav

--- index.js
+++ index.js
@@ -1,4 +1,50 @@
-document.addEventListener('DOMContentLoaded', function() {
+document.addEventListener('DOMContentLoaded', function () {
+    
+
+
+
+        document.querySelector(".header").classList.remove("open");
+        document.body.classList.remove("menu-open");
+       
+        document.getElementById("burger").addEventListener("click", () => {
+            window.scrollTo({
+                top: 0,
+            });
+            document.querySelector(".header").classList.toggle("open");
+            document.body.classList.toggle("menu-open");
+        });
+       
+        // document.querySelectorAll(".header__menu-link").forEach((link) => {
+            link.addEventListener("click", (event) => {
+                event.preventDefault();
+                const targetUrl = link.getAttribute("href");
+       
+                document.querySelector(".header").classList.remove("open");
+                document.body.classList.remove("menu-open");
+       
+                setTimeout(() => {
+                    window.location.href = targetUrl;
+                }, 300);
+            });
+        // });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
             const cards = document.querySelectorAll('.card');
             const slider = document.querySelector('.cards-accordion');
             const actionButton = document.getElementById('actionButton');
